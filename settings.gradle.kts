d7416f4cc3d80956809450ffa958027ba722220e
mahyazz <mahya.zarrabi@gmail.com>
2025-09-12 19:42:47 +0330

    Update settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,35 +1,36 @@
 pluginManagement {
     repositories {
-        // --- میرورها (اختیاری: اگر گوگل اصلی کند/مسدود است) ---
-        maven{url = uri("https://maven.myket.ir")}
-        maven { url = uri("https://maven.aliyun.com/repository/google") }       // آینه‌ی Google Maven
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") } // آینه‌ی پلاگین‌ها
-        maven { url = uri("https://maven-central-asia.storage-download.googleapis.com/maven2") } // آینه‌ی Maven Central (APAC)
-
-        // --- منابع رسمی (همیشه نگه‌دار) ---
-        google()
+        if (System.getProperty("useMirrors") != null) {
+            maven{url = uri("https://maven.myket.ir")}
+            maven { url = uri("https://maven.aliyun.com/repository/google") }
+            maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+            maven { url = uri("https://maven-central-asia.storage-download.googleapis.com/maven2") }
+        }
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
-
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        // --- میرورها (در صورت نیاز) ---
-        maven { url = uri("https://maven.aliyun.com/repository/google") }        // فقط اگر Google اصلیت مشکل دارد
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-        maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
-        maven { url = uri("https://maven-central-asia.storage-download.googleapis.com/maven2") }
-
-        // --- رسمی‌ها ---
+        if (System.getProperty("useMirrors") != null) {
+            maven { url = uri("https://maven.aliyun.com/repository/google") }
+            maven { url = uri("https://maven.aliyun.com/repository/public") }
+            maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+            maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
+            maven { url = uri("https://maven-central-asia.storage-download.googleapis.com/maven2") }
+        }
         google()
         mavenCentral()
     }
 }
-
 
 rootProject.name = "SimpleNote"
 include(":app")
