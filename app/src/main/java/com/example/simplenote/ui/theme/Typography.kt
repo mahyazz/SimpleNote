02b111e31ba6cdf83c3b5a67558a480ae35b4880
mousavi2003 <94689497+mousavi2003@users.noreply.github.com>
2025-09-13 19:41:09 +0330

    adding new simplenot

--- app/src/main/java/com/example/simplenote/ui/theme/Typography.kt
+++
@@ -1,6 +0,0 @@
-package com.example.simplenote.ui.theme
-
-import androidx.compose.material3.Typography
-
-// ستینگ تایپوگرافی مخصوص اپ (الان همون پیش‌فرض متریال3ه)
-val AppTypography = Typography()
